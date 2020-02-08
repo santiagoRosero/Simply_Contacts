3664ddd0affb8ce32500006fb7ba04c07c0f170e
JuanDVera <juandaveru@gmail.com>
2020-02-08 11:58:27 -0500

    course now has its attributes

--- src/model/Course.java
+++ src/model/Course.java
@@ -4,10 +4,22 @@ public class Course {
 	
 	/**The number of credits this course has.*/
 	private int credits;
+
+	/** The NRC is used to identify a course */
+	private int NRC;
+	
+	/** Name of the course. */
+	private String name;
+	
+	/** Short Description of the course */
+	private String description;
 	
 	/**Returns the number of credits this course has.
 	 * @return The value of the field credits.
 	 */
+	
+	
+	
 	public int getCredits() {
 		return credits;
 	}
